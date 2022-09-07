e3a6e40351266d6cf819983f0370f68e230f806e
Adrian Ordonez <adrianord@gmail.com>
2022-09-07 10:56:16 -0700

    refactor(*): reorganize module files into programs folder

--- modules/default.nix
+++ modules/default.nix
@@ -2,14 +2,8 @@
   nixos = ./nixos;
   darwin = ./darwin;
   standard = [
-    ./vscode
-    ./kubernetes.nix
     ./languages
-    ./neovim.nix
-    ./zsh.nix
-    ./git.nix
-    ./firefox.nix
-    ./direnv.nix
+    ./programs
   ];
   personal = [
     ./personal
---
+++ modules/programs/default.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  imports = [
+    ./direnv.nix
+    ./firefox.nix
+    ./git.nix
+    ./kubernetes.nix
+    ./neovim.nix
+    ./vscode
+    ./zsh.nix
+  ];
+}
